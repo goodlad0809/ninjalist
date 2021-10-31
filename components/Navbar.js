473dad400e999a6e01ee0b81a972f7b85795e713
goodlad0809 <UjBg9Beso%Ge>
2021-10-31 16:25:24 -0700

    updated logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,27 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
-    return ( 
-        <nav>
-            <div className="logo">
-            <Image src="/feedback.png" width={32} height={32}/>
-            </div>
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/about"><a>About</a></Link>
-            <Link href="/ninjas"><a>Ninja List</a></Link>
-        </nav>
-     );
-}
- 
+  return (
+    <nav>
+      <div className="logo">
+        <Link href="/">
+          <a>
+            <Image src="/feedback.png" width={32} height={32} />
+          </a>
+        </Link>
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>Ninja List</a>
+      </Link>
+    </nav>
+  );
+};
+
 export default Navbar;
